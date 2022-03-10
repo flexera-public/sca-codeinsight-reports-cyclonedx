4ffebf44022409fea2b6e3e2d088667dbd0c9169
Sean Geary <sgeary@flexera.com>
2022-03-10 16:26:37 -0500

    Initial commit

---
+++ _version.py
@@ -0,0 +1,10 @@
+'''
+Copyright 2022 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary  
+Created On : Thu Mar 10 2022
+File : _version.py
+'''
+__version__ = "1.0.1"
